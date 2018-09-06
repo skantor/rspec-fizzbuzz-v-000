cc4cdd58e04485e86f0925625ad901a9838558c8
Sara Kantor <sara.kantor.nv@gmail.com>
2018-09-06 00:25:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
-  elseif int % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  elseif int %5 == 0 && int % 3 == 0
+  elsif int %5 == 0 && int % 3 == 0
     puts "FizzBuzz"
   else
     nil
